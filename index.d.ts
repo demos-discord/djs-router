b27795432c9fab391d61d7df8ed17353b750a386
Andy <andranadu@gmail.com>
2018-09-15 11:22:00 +0100

    Mapping discord classes to generic data in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,58 @@
 
 declare module "djs-router" {
 
+    import * as Discord from 'discord.js';
+
     export class RouterData {
         constructor();
+        channel: Discord.Channel;
+        oldChannel: Discord.Channel;
+        newChannel: Discord.Channel;
+
+        clientUserGuildSettings: Discord.ClientUserGuildSettings;
+        clientUserSettings: Discord.ClientUserSettings;
+
+        info: string;
+
+        emoji: Discord.Emoji;
+        oldEmoji: Discord.Emoji;
+        newEmoji: Discord.Emoji;
+
+        error: Error;
+
+        guild: Discord.Guild;
+        oldGuild: Discord.Guild;
+        newGuild: Discord.Guild;
+
+        user: Discord.User | Discord.UserResolvable;
+        oldUser: Discord.User;
+        newUser: Discord.User;
+
+        member: Discord.GuildMember;
+        oldMember: Discord.GuildMember;
+        newMember: Discord.GuildMember;
+        members: Discord.GuildMember[];
+
+        speaking: boolean;
+        time: Date;
+
+        message: Discord.Message;
+        oldMessage: Discord.Message;
+        newMessage: Discord.Message;
+        messages: Discord.Collection<Discord.Snowflake, Discord.Message>;
+
+        messageReaction: Discord.MessageReaction;
+        rateLimit: Discord.RateLimitInfo;
+        replayed: number;
+
+        role: Discord.Role;
+        oldRole: Discord.Role;
+        newRole: Discord.Role;
+
+        oldNote: string;
+        newNote: string;
+        [key: string]: any;
+
     }
 
     export class RouteStop {
